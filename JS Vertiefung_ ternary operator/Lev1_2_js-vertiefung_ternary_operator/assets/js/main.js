9931ae52aad5d2a591fd40c0d226c83235b6a320
Maartala <steffi.gerstenmaier@t-online.de>
2023-03-09 15:31:04 +0100

    unnötige Kommentare gelöscht

--- JS Vertiefung_ ternary operator/Lev1_2_js-vertiefung_ternary_operator/assets/js/main.js
+++ JS Vertiefung_ ternary operator/Lev1_2_js-vertiefung_ternary_operator/assets/js/main.js
@@ -4,13 +4,4 @@ function check(){
 
     let password = document.getElementById('password').value.length > 8 == true
     password ?  result.innerHTML = "<span class='welcome'> Welcome to your Account </span>" : result.innerHTML = "<span class='short'>Your passwort is short </span>" 
-
 }
-
-
-    // password = document.getElementById('password').value.length > 8 == true
-
-    // password ?  result.style.color = 'green' : result.innerHTML = result.style.color = 'red'
-
-    // let res = income >= outcome ? "green" : "red";
-    // document.getElementById("currency").style.color = res;
