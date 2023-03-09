2d6db0f68bfa73a5c6d4c802672d67695e9ca1e4
Maartala <steffi.gerstenmaier@t-online.de>
2023-03-09 15:13:29 +0100

    mit Farben beim Ergebnis

--- JS Vertiefung_ ternary operator/Lev1_2_js-vertiefung_ternary_operator/assets/js/main.js
+++ JS Vertiefung_ ternary operator/Lev1_2_js-vertiefung_ternary_operator/assets/js/main.js
@@ -3,6 +3,14 @@ function check(){
     let result = document.querySelector('#result');
 
     let password = document.getElementById('password').value.length > 8 == true
+    password ?  result.innerHTML = "<span class='welcome'> Welcome to your Account </span>" : result.innerHTML = "<span class='short'>Your passwort is short </span>" 
 
-    password ?  result.innerHTML = "Welcome to your Account" : result.innerHTML = "Your passwort is short" 
 }
+
+
+    // password = document.getElementById('password').value.length > 8 == true
+
+    // password ?  result.style.color = 'green' : result.innerHTML = result.style.color = 'red'
+
+    // let res = income >= outcome ? "green" : "red";
+    // document.getElementById("currency").style.color = res;
